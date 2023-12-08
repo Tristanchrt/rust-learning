130c8660f2911219b6455ffdd64986bfe0fdbdb8
tch <tristan.chretien@tenacy.io>
2023-12-08 13:44:20 +0100

    feat: enum

---
+++ src/enums.rs
@@ -0,0 +1,94 @@
+
+#[derive(Debug)]
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+struct QuitMessage; // unit struct
+struct MoveMessage {
+    x: i32,
+    y: i32,
+}
+struct WriteMessage(String); // tuple struct
+struct ChangeColorMessage(i32, i32, i32); // tuple struct
+
+impl Message {
+    fn call(&self) {
+        dbg!();
+        match self {
+            Message::Write(content) => println!("Content of Write variant: {}", content),
+            Message::Move { x, y } => println!("Move variant: x={}, y={}", x, y),
+            _ => println!("Variant does not contain content to display."),
+
+        }
+      
+    }
+}
+
+enum Option<T> {
+    None,
+    Some(T),
+}
+
+fn get_value(condition: bool) -> Option<i32> {
+    if condition {
+        Option::Some(42)
+    } else {
+        Option::None
+    }
+}
+
+#[derive(Debug)] // so we can inspect the state in a minute
+enum UsState {
+    Alabama,
+    Alaska(String)
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        },
+        _ => 10
+    }
+}
+
+fn main() {
+
+    // let m = Message::Write(String::from("hello"));
+    // m.call();
+    
+    // let pomme = MoveMessage {
+    //     x: 10,
+    //     y: 20,
+    // };
+
+    // let a = match pomme {
+    //     MoveMessage { x, y } => Message::Move { x, y },
+    // };
+    // a.call();
+
+    let result = get_value(true);
+
+    match result {
+        Option::Some(value) => println!("Value is {}", value),
+        Option::None => println!("No value present"),
+    }
+
+    let toto = Coin::Quarter(UsState::Alaska(String::from("hello")));
+    value_in_cents(toto);
+    
+}
--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,7 @@
-#[derive(Debug)]
-struct Rectangle {
-    width: u32,
-    height: u32,
-}
-impl Rectangle {
-    fn area(&self) -> u32 {
-        self.width * self.height
-    }
-}
 
-impl Rectangle {
-    fn can_hold(&self, other: &Rectangle) -> bool {
-        self.width > other.width && self.height > other.height
-    }
-    // Static method
-    fn square(size: u32) -> Self {
-        Self {
-            width: size,
-            height: size,
-        }
-    }
-}
 fn main() {
-    let rect1 = Rectangle {
-        width: 100,
-        height: 50,
-    };
 
-    println!(
-        "The area of the rectangle is {} square pixels.",
-        rect1.area(),
-    );
-
-
-    println!(
-        "The area of the rectangle is {} square pixels.",
-        rect1.can_hold(&rect1)
-    );
+    println!("toto ")
+    
+    
 }
